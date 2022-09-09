fe04f30aa0597332842821d6c7047e364d0e819b
Chrisnisch <mihail.gusev.95@yandex.ru>
2022-09-09 16:45:01 +0300

    lesson 2 passed

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -48,16 +48,14 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
 fun daysInMonth(month: Int, year: Int): Int {
-    val ev = 30
-    val odd = 31
-    if (month != 2 && month != 8) {
-        return when {
-            month % 2 == 0 -> ev
-            else -> odd
-        }
-    } else if (month == 8) return odd
-    return if (year % 100 != 0 && year % 4 == 0 || year % 400 == 0) 29
-    else 28
+    val l1 = arrayOf<Int>(1, 3, 5, 7, 8, 10, 12)
+    val l2 = arrayOf<Int>(4, 6, 9, 11)
+    if (month == 2 && (year % 100 != 0 && year % 4 == 0 || year % 400 == 0)) return 29
+    return when (month) {
+        in l1 -> 31
+        in l2 -> 30
+        else -> 28
+    }
 }
 
 /**
